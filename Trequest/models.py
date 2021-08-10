b1f42b0cbfe054f39156513f1157ad5f38bcf270
roberadanie <danielrobera9@gmail.com>
2021-08-10 15:10:01 -0400

    feedback by roba

--- Trequest/models.py
+++ Trequest/models.py
@@ -9,23 +9,26 @@ from TSERASP import settings
 
 # defining the users school name
 class School(models.Model):
-    name=models.CharField(max_length=100,null=True,blank=True,unique=True)
+    name = models.CharField(max_length=100, null=True, blank=True, unique=True)
 
     def __str__(self):
         return self.name
 
 # defining the users department
+
+
 class Department(models.Model):
-    school=models.ForeignKey(School,on_delete=models.CASCADE)
-    name=models.CharField(max_length=100,null=True,blank=True,unique=True)
+    school = models.ForeignKey(School, on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, null=True, blank=True, unique=True)
 
     def __str__(self):
         return self.name
 
+
 class MyUser(AbstractUser):
     first_name = models.CharField(max_length=100, null=True)
     last_name = models.CharField(max_length=100, null=True)
-    email = models.EmailField(null=True,blank=True)
+    email = models.EmailField(null=True, blank=True)
     ROLE = (
         ('Passenger', 'Passenger'),
         ('TSHO', 'TSHO'),
@@ -63,11 +66,14 @@ class MyUser(AbstractUser):
         regex=r'^\+?1?\d{10,15}$',
         message='Phone number must be entered in the format : 0987654321 or +251987654321 up to 15 digits allowed'
     )
-    phone = models.CharField(validators=[phone_regex], max_length=15, blank=True)
-    school = models.ForeignKey(School,on_delete=models.SET_NULL,blank=True,null=True)
-    department = models.ForeignKey(Department,on_delete=models.SET_NULL,blank=True,null=True)
+    phone = models.CharField(
+        validators=[phone_regex], max_length=15, blank=True)
+    school = models.ForeignKey(
+        School, on_delete=models.SET_NULL, blank=True, null=True)
+    department = models.ForeignKey(
+        Department, on_delete=models.SET_NULL, blank=True, null=True)
     role = models.CharField(max_length=200, choices=ROLE)
-    date_registered = models.DateField(auto_now_add=True,null=True)
+    date_registered = models.DateField(auto_now_add=True, null=True)
 
     def __str__(self):
         return self.username
@@ -76,18 +82,22 @@ class MyUser(AbstractUser):
 
 
 class Profile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='passenger')
-    sex = models.CharField(max_length=200, choices=(('male', 'male'), ('female', 'female')), null=True)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='passenger')
+    sex = models.CharField(max_length=200, choices=(
+        ('male', 'male'), ('female', 'female')), null=True)
     bio = models.TextField(max_length=500, blank=True)
     location = models.CharField(max_length=30, blank=True)
     birth_date = models.DateField(null=True, blank=True)
     # image = models.ImageField(null=True,blank=True)
 
     def __str__(self):
-        return self.user.first_name + "  "+ self.user.last_name + "  " + 'detail'
+        return self.user.first_name + "  " + self.user.last_name + "  " + 'detail'
+
 
 class Driver(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='employee')
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='employee')
     occupation = models.CharField(max_length=200, null=True)
     address = models.CharField(max_length=100, null=True)
     house_no = models.CharField(max_length=100, null=True)
@@ -101,38 +111,48 @@ class TransportRequest(models.Model):
         ('Pending', 'Pending'),
         ('Approved', 'Approved'),
     )
-    passenger = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='user_request')
+    passenger = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='user_request')
     start_from = models.CharField(max_length=200)
     destination = models.CharField(max_length=200)
-    reason = RichTextField(blank=True,null=True)
+    reason = RichTextField(blank=True, null=True)
     passenger_numbers = models.PositiveIntegerField(default=1)
-    list_of_passengers=models.TextField(max_length=400,null=True,blank=True)
+    list_of_passengers = models.TextField(
+        max_length=400, null=True, blank=True)
     start_date = models.DateTimeField()
     end_date = models.DateTimeField()
     created_at = models.DateTimeField(auto_now_add=True)
-    status = models.CharField(max_length=200, default='Pending', choices=STATUS)
-    status2 = models.CharField(max_length=200, default='Pending', choices=STATUS)
-    status3 = models.CharField(max_length=200, default='Pending', choices=STATUS)
+    status = models.CharField(
+        max_length=200, default='Pending', choices=STATUS)
+    status2 = models.CharField(
+        max_length=200, default='Pending', choices=STATUS)
+    status3 = models.CharField(
+        max_length=200, default='Pending', choices=STATUS)
 # status=TSHO
 # status2=department
 # status3=School
+
     def __str__(self):
         return self.start_from + ' to ' + self.destination
-    
+
     class Meta:
-        ordering = ['-id'] 
+        ordering = ['-id']
 
 # notifications for viewing new request
+
+
 class Notifications(models.Model):
     # sender_id=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
-    request_id=models.ForeignKey(TransportRequest,on_delete=models.CASCADE,related_name='trequest')
-    is_viewed=models.BooleanField(default=False)
-    from_who=models.ForeignKey(TransportRequest, on_delete=models.CASCADE,null=True,related_name='from_who')
-
+    request_id = models.ForeignKey(
+        TransportRequest, on_delete=models.CASCADE, related_name='trequest')
+    is_viewed = models.BooleanField(default=False)
+    from_who = models.ForeignKey(
+        TransportRequest, on_delete=models.CASCADE, null=True, related_name='from_who')
 
     def __str__(self):
         return str(self.request_id.passenger.first_name)
-    
+
+
 class Vehicle(models.Model):
     STATUS = (
         ('Not Occupied', 'Not Occupied'),
@@ -148,22 +168,27 @@ class Vehicle(models.Model):
         ('kobra', 'kobra'),
         ('ambulance', 'ambulance')
     )
-    adder = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='add')
+    adder = models.ForeignKey(settings.AUTH_USER_MODEL,
+                              on_delete=models.CASCADE, related_name='add')
     vehicle_type = models.CharField(max_length=200, choices=type_of_vehicle)
     plate_number = models.CharField(max_length=20, unique=True)
-    status = models.CharField(max_length=200, choices=STATUS, default='Not Occupied')
-    driver = models.OneToOneField(Driver, on_delete=models.CASCADE, null=True,blank=True)
+    status = models.CharField(
+        max_length=200, choices=STATUS, default='Not Occupied')
+    driver = models.OneToOneField(
+        Driver, on_delete=models.CASCADE, null=True, blank=True)
     date_entered = models.DateTimeField(auto_now_add=True, null=True)
-    currently=models.CharField(max_length=200,choices=current,default='Inside')
+    currently = models.CharField(
+        max_length=200, choices=current, default='Inside')
 
     def __str__(self):
         return self.plate_number
+
 
 class ApproveRequest(models.Model):
     user = models.OneToOneField(TransportRequest, on_delete=models.CASCADE)
     vehicle = models.OneToOneField(Vehicle, on_delete=models.CASCADE)
     time = models.CharField(max_length=200, default='06:00am')
-    
+
     def __str__(self):
         return self.user.start_from + ' to ' + self.user.destination + ' Approved '
 
@@ -174,9 +199,11 @@ class Schedule(models.Model):
         ('Afternoon', 'Afternoon'),
         ('Evening', 'Evening'),
     )
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='adder')
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='adder')
     shift = models.CharField(max_length=100, choices=select)
-    driver = models.ForeignKey(Driver, on_delete=models.CASCADE, related_name='driver_name')
+    driver = models.ForeignKey(
+        Driver, on_delete=models.CASCADE, related_name='driver_name')
     service_type = models.CharField(max_length=200)
     date = models.DateField()
     time = models.CharField(max_length=200)
@@ -185,14 +212,16 @@ class Schedule(models.Model):
     def __str__(self):
         return self.service_type
 #     by naol
+
+
 class Material(models.Model):
-    user = models.ForeignKey(settings.AUTH_USER_MODEL,null=True, on_delete= models.SET_NULL)
-    name=models.CharField(max_length=200)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             null=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=200)
     type_of = models.CharField(max_length=200)
-    quantity=models.PositiveIntegerField()
-    date_created = models.DateField( auto_now_add=  True, null=True)
+    quantity = models.PositiveIntegerField()
+    date_created = models.DateField(auto_now_add=True, null=True)
     updated_at = models.DateTimeField(null=True)
-    
 
     def __str__(self):
         return self.name
@@ -204,36 +233,50 @@ class MaterialRequest(models.Model):
         ('Pending', 'Pending'),
         ('Approved', 'Approved'),
     )
-    new_material_name = models.ForeignKey(Material, on_delete=models.DO_NOTHING, null=True, related_name='new_material')
+    new_material_name = models.ForeignKey(
+        Material, on_delete=models.DO_NOTHING, null=True, related_name='new_material')
     new_material_model = models.CharField(max_length=200)
     quantity_of_new = models.PositiveIntegerField()
-    old_material_name = models.ForeignKey(Material, on_delete=models.DO_NOTHING, null=True, related_name='old_material')
-    quantity_of_old=models.PositiveIntegerField()
-    old_material_model=models.CharField(max_length=200)
-    vehicle_model = models.ForeignKey(Vehicle, max_length=200, on_delete=models.DO_NOTHING, null=True)
-    condition = models.CharField(max_length=200,choices=(('Reusable', 'reusable'), ('Usable', 'usable')), null=True)
-    status = models.CharField(max_length=200, default='Pending', choices=STATUS)
-
+    old_material_name = models.ForeignKey(
+        Material, on_delete=models.DO_NOTHING, null=True, related_name='old_material')
+    quantity_of_old = models.PositiveIntegerField()
+    old_material_model = models.CharField(max_length=200)
+    vehicle_model = models.ForeignKey(
+        Vehicle, max_length=200, on_delete=models.DO_NOTHING, null=True)
+    condition = models.CharField(max_length=200, choices=(
+        ('Reusable', 'reusable'), ('Usable', 'usable')), null=True)
+    status = models.CharField(
+        max_length=200, default='Pending', choices=STATUS)
 
     def __str__(self):
         return self.new_material_name.name
 # evaluate Driver
+
+
 class DriverEvaluation(models.Model):
-    select=(
-        ('1',1),
-        ('2',2),
-        ('3',3),
-        ('4',4),
-        ('5',5),
+    select = (
+        ('1', 1),
+        ('2', 2),
+        ('3', 3),
+        ('4', 4),
+        ('5', 5),
 
     )
-    driver = models.ForeignKey(Driver, on_delete=models.CASCADE, related_name='drivers_name')
-    rating=models.CharField(max_length=1,choices=select)
-    duser = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='evaluator')
-    trip = models.ForeignKey(TransportRequest,on_delete=models.CASCADE, related_name='trip_name',null=True)
+    driver = models.ForeignKey(
+        Driver, on_delete=models.CASCADE, related_name='drivers_name')
+    rating = models.CharField(max_length=1, choices=select)
+    duser = models.ForeignKey(settings.AUTH_USER_MODEL,
+                              on_delete=models.CASCADE, related_name='evaluator')
+    trip = models.ForeignKey(
+        TransportRequest, on_delete=models.CASCADE, related_name='trip_name', null=True)
 
-    
 
+# feedback
+class feedback(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,
+                             on_delete=models.CASCADE)
+    message = models.TextField(max_length=1000)
+    sent_at = models.DateTimeField(auto_now_add=True)
 
 
 # class Material(models.Model):
--- Trequest/urls.py
+++ Trequest/urls.py
@@ -1,16 +1,16 @@
-from django.urls import path,re_path
+from django.urls import path, re_path
 # for password reset
-from django.contrib.auth import views as auth_views 
+from django.contrib.auth import views as auth_views
 
 from . import views
 urlpatterns = [
     path('', views.signin, name='login'),
     path('home/', views.index, name='index'),
     # re_path(r'/', views.notifications, name='notify'),
-    #pdf generation naol
-   # path('pdf/', views.render_pdf_view, name='pdf'),   
-    #path('Pdf/', views.Requestpdf.as_view(), name='pdf'),   
-    path('Pdf/<pk>/', views.request_pdf, name='request-pdf'),   
+    # pdf generation naol
+    # path('pdf/', views.render_pdf_view, name='pdf'),
+    #path('Pdf/', views.Requestpdf.as_view(), name='pdf'),
+    path('Pdf/<pk>/', views.request_pdf, name='request-pdf'),
 
 
     # account related
@@ -18,31 +18,44 @@ urlpatterns = [
     path('profile/', views.profile, name='profile'),
     path('account/manage', views.account_management, name='account'),
     path('account/delete/<int:id>', views.delete_account, name='delete-account'),
-    path('account/edit/',views.edit_account,name='edit-account'),
-    path('account-detail/<str:username>', views.account_detail, name='detail-account'),
+    path('account/edit/', views.edit_account, name='edit-account'),
+    path('account-detail/<str:username>',
+         views.account_detail, name='detail-account'),
     path('logout/', views.user_logout, name='logout'),
-    path('change_password/',views.change_password,name='change-password'),
-    path('ajax/load_department',views.load_department,name='ajax_load_department'),
-  
-    #vehicle request related
+    path('change_password/', views.change_password, name='change-password'),
+    path('ajax/load_department', views.load_department,
+         name='ajax_load_department'),
+
+    # vehicle request related
     path('request/tshoview', views.tsho_view_request, name="tsho-view-request"),
-    path('request/tshoapproved', views.tsho_view_approved_request, name="tsho-view-approved-request"),
-    path('request/departmentview', views.department_view_request, name="department-view-request"),
-    path('request/departmentdapproved', views.department_view_approved_request, name="department-view-approved-request"),
-    path('request/departmentapprove/<int:id>', views.department_approve_request, name="department-approve-request"),
-    path('request/tshoapproved/detail/<str:id>', views.tsho_view_approved_request_detail, name="view-detail"),
-    path('request/schoolview', views.school_view_request, name="school-view-request"),
-    path('request/schoolapproved', views.school_view_approved_request, name="school-view-approved-request"),
-    path('request/schoolapprove/<int:id>', views.school_approve_request, name="school-approve-request"),
+    path('request/tshoapproved', views.tsho_view_approved_request,
+         name="tsho-view-approved-request"),
+    path('request/departmentview', views.department_view_request,
+         name="department-view-request"),
+    path('request/departmentdapproved', views.department_view_approved_request,
+         name="department-view-approved-request"),
+    path('request/departmentapprove/<int:id>',
+         views.department_approve_request, name="department-approve-request"),
+    path('request/tshoapproved/detail/<str:id>',
+         views.tsho_view_approved_request_detail, name="view-detail"),
+    path('request/schoolview', views.school_view_request,
+         name="school-view-request"),
+    path('request/schoolapproved', views.school_view_approved_request,
+         name="school-view-approved-request"),
+    path('request/schoolapprove/<int:id>',
+         views.school_approve_request, name="school-approve-request"),
     path('request/my_request', views.my_request, name="my-request"),
-    path('request/my_request/detail/<str:id>', views.my_request_detail, name="detail-request"),
+    path('request/my_request/detail/<str:id>',
+         views.my_request_detail, name="detail-request"),
     path('request/make', views.make_request, name="make-request"),
     path('request/cancel/<int:id>', views.cancel_request, name="cancel-request"),
-    path('request/tshoapprove/<str:id>', views.tsho_approve_request, name="tsho-approve-request"),
-    path('vehicle/repaired',views.repaired_vehicle,name='repaired-vehicle'),
+    path('request/tshoapprove/<str:id>',
+         views.tsho_approve_request, name="tsho-approve-request"),
+    path('vehicle/repaired', views.repaired_vehicle, name='repaired-vehicle'),
 
     # material request
-    path('request/materialrequest', views.material_request, name="material-request"),
+    path('request/materialrequest',
+         views.material_request, name="material-request"),
 
     # vehicle  related
     path('vehicle/', views.vehicle_management, name="vehicle-manage"),
@@ -62,18 +75,26 @@ urlpatterns = [
     path('deletematerial/<str:pk>/', views.deletematerial, name="delete_material"),
 
     # history
-     path('history/', views.history, name="history"),
+    path('history/', views.history, name="history"),
 
     #  password reset
-    path('password_reset/',auth_views.PasswordResetView.as_view(template_name="Trequest/password_reset_form.html"),name='password-reset'),
-    path('password_reset/done/',auth_views.PasswordResetDoneView.as_view(template_name="Trequest/password_reset_done.html"),name='password_reset_done'),
-    path('reset/<uidb64>/<token>/',auth_views.PasswordResetConfirmView.as_view(template_name="Trequest/password_reset_confirm.html"),name='password_reset_confirm'),
-    path('reset/done/',auth_views.PasswordResetCompleteView.as_view(template_name="Trequest/password_reset_complete.html"),name='password_reset_complete'),
-     
-     # Driver Evaluation
-     path('evaluate/', views.evaluate, name="evaluate-driver"),
-    
+    path('password_reset/', auth_views.PasswordResetView.as_view(
+        template_name="Trequest/password_reset_form.html"), name='password-reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(
+        template_name="Trequest/password_reset_done.html"), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name="Trequest/password_reset_confirm.html"), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name="Trequest/password_reset_complete.html"), name='password_reset_complete'),
+
+    # Driver Evaluation
+    path('evaluate/', views.evaluate, name="evaluate-driver"),
+
     # report
-    path('report/',views.report, name="report"),
- 
+    path('report/', views.report, name="report"),
+
+    # feedback
+    path('feedback/create_feedback', views.FeedBack, name="create-feedback"),
+    path('feedback/view_feedback', views.view_feedback, name="view-feedback"),
+
 ]
