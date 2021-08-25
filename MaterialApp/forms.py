98f390518919afa06832f458becf7a33ccdbbd48
Your Name <you@example.com>
2021-08-25 21:28:52 +0300

    alert

--- MaterialApp/forms.py
+++ MaterialApp/forms.py
@@ -4,7 +4,7 @@ from .models import Material, MaterialRequest
 class AddMaterialForm(forms.ModelForm):
     class Meta:
         model = Material
-        fields = ['name', 'type_of', 'quantity']
+        fields = ['name', 'type_of', 'quantity', 'model']
 
 class MaterialRequestForm(forms.ModelForm):
     class Meta:
